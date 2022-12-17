748bd7da03b651ea3a0d46f65de2e36229e00a9a
FloreDenis2001 <floredenis907@yahoo.com>
2022-12-17 04:32:43 +0200

    last_update

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ let paginationSection=document.querySelector(".pagination");
 let cardsSection=document.querySelector(".cards");
 let asideContainer=document.querySelector(".modal");
 let asideEdit=document.querySelector(".edit-container");
-
-
+let searchZone=document.querySelector(".search-container");
 createButtons(data,6);
 createPage(1);
 
@@ -21,9 +20,10 @@ paginationSection.addEventListener("click",(e)=>{
 
 cardsSection.addEventListener("click",(e)=>{
     let obj=e.target;
-    if(obj.classList.contains('card')){
+    if(obj.classList.contains('img-card')){
+        let container=obj.parentNode;
         asideContainer.style.display='flex';
-        let findObject=findObjectByEmail(data,obj.children[2].textContent);
+        let findObject=findObjectByEmail(data,container.children[2].textContent);
         asideContainer.appendChild(createModal(findObject));
     }
 })
@@ -39,33 +39,84 @@ asideContainer.addEventListener("click",(e)=>{
         asideEdit.style.display='flex';   
         let container=obj.parentNode.parentNode;
         let findObject=findObjectByEmail(data,container.children[3].textContent);
-        console.log(editCreate(findObject));
         asideEdit.appendChild(editCreate(findObject));
     }else if(obj.classList.contains('delete-modal')){
         let container=obj.parentNode.parentNode;
         let findObject=findObjectByEmail(data,container.children[3].textContent);
-        removeCard(data,findObject);
-        createButtons(data,6);
-      
+        data=removeByEmail(data,findObject.email);
+        cardsSection.innerHTML=" ";
+        createPage(1);
+        asideContainer.style.display='none';
+        asideContainer.removeChild(asideContainer.children[0]);
+    }else if(obj.classList.contains('prevBtn')){
+        let containerPrev=obj.parentNode;
+        let positionData=findPositionByEmail(data,containerPrev.children[3].textContent);
+        if(data.length>=positionData-1 && positionData-1>=0){
+        asideContainer.appendChild(createModal(data[positionData-1]));
+        asideContainer.removeChild(asideContainer.children[0]);
+        }else {
+            alert("ACESTA ESTE PRIMUL ELEMENT");
+            asideContainer.appendChild(createModal(data[data.length-1]));
+            asideContainer.removeChild(asideContainer.children[0]);
+        }
+    }else if(obj.classList.contains('nextBtn')){
+        let containerPrev=obj.parentNode;
+        let positionData=findPositionByEmail(data,containerPrev.children[3].textContent);
+        if(data.length-1>=positionData+1){
+        asideContainer.appendChild(createModal(data[positionData+1]));
+        asideContainer.removeChild(asideContainer.children[0]);
+        }else{
+            alert("ULTIMUL ELEMENT A FOST VAZUT");
+            asideContainer.appendChild(createModal(data[0]));
+            asideContainer.removeChild(asideContainer.children[0]);
+        }
     }
 })
 
 
 asideEdit.addEventListener("click",(e)=>{
     let obj=e.target;
+    let container=obj.parentNode.parentNode;
+    let positionData=findPositionByEmail(data,container.children[2].textContent);
+    let containerImg=container.children[0];
+    let containerInputImg=containerImg.children[1];
+
+    
     if(obj.classList.contains("cancel-btn")){
         asideEdit.style.display='none'; 
         asideContainer.style.display='flex';
         asideEdit.innerHTML=" ";
     }else if(obj.classList.contains("save-btn")){
-        let container=obj.parentNode.parentNode;
-        console.log(container);
-        let findObject=findObjectByEmail(data,container.children[2].value);
-        console.log(findObject);
         let valueName=container.children[1].value;
-        let valueEmail=container.children[2].value;
-        updateName(findObject,valueName);
-        findObject.email=valueEmail;
-        console.log(valueEmail+" "+valueName);
+        if(valueName.value!=""){
+        updateName(data,positionData,valueName);
+        }
+    
+        let valueImg=containerInputImg.children[1].value;
+        if(valueImg.value){
+        updateImg(data,positionData,valueImg);
+        }
+
+
+
+        cardsSection.innerHTML=" ";
+        createPage(1);
+        asideEdit.style.display='none'; 
+        asideEdit.innerHTML=" "; 
+        asideContainer.style.display='none';
+        asideContainer.removeChild(asideContainer.children[0]);
     }
+})
+
+searchZone.addEventListener("click",(e)=>{
+    let obj=e.target;
+    if(obj.classList.contains('search-zone')){
+        let inputValue=searchZone.children[0].value;
+        if(findByName(data,inputValue)!=null){
+        asideContainer.style.display='flex';
+        asideContainer.appendChild(createModal(findByName(data,inputValue)));
+    }else {
+        alert("Numele introdus incorect");
+    }
+}
 })
